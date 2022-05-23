78411b231339ca81b190d79d1ed9cb30a566994e
Abhishek <abhishek.shivakumar@gmail.com>
2022-05-23 12:31:50 +0100

    Statistics:   Added VectorOperations

--- Statistics.h
+++ Statistics.h
@@ -9,13 +9,56 @@
 #include <cmath>
 
 
-//Additional Vector operations should happen here
-inline std::vector<float> operator-(std::vector<float> a, float b)
+//Maybe move all of this to a vectorOperations.h file somewhere.
+//Vector and float operations
+inline std::vector<float> operator- (std::vector<float> a, float b)
 {
     std::vector<float> retvect;
     for (int i = 0; i < a.size(); i++)
     {
-        retvect.push_back(a[i] - b);
+        retvect.push_back  (a[i] - b);
+    }
+    return retvect;
+}
+
+inline std::vector<float> operator+ (std::vector<float> a, float b)
+{
+    std::vector<float> retvect;
+    for (int i = 0; i < a.size(); i++)
+    {
+        retvect.push_back  (a[i] + b);
+    }
+    return retvect;
+}
+
+inline std::vector<float> operator* (std::vector<float> a, float b)
+{
+    std::vector<float> retvect;
+    for (int i = 0; i < a.size(); i++)
+    {
+        retvect.push_back  (a[i] * b);
+    }
+    return retvect;
+}
+
+inline std::vector<float> operator/ (std::vector<float> a, float b)
+{
+    std::vector<float> retvect;
+    for (int i = 0; i < a.size(); i++)
+    {
+        retvect.push_back  (a[i] / b);
+    }
+    return retvect;
+}
+
+
+//Vector and Vector Operations
+inline std::vector<float> operator- (std::vector<float> a, std::vector<float> b)
+{
+    std::vector<float> retvect;
+    for (int i = 0; i < a.size(); i++)
+    {
+        retvect.push_back  (a[i] - b[i]);
     }
     return retvect;
 }
@@ -25,7 +68,27 @@ inline std::vector<float> operator* (std::vector<float> a, std::vector<float> b)
     std::vector<float> retvect;
     for (int i = 0; i < a.size() ; i++)
     {
-        retvect.push_back(a[i] * b[i]);
+        retvect.push_back (a[i] * b[i]);
+    }
+    return retvect;
+}
+
+inline std::vector<float> operator/ (std::vector<float> a, std::vector<float> b)
+{
+    std::vector<float> retvect;
+    for (int i = 0; i < a.size() ; i++)
+    {
+        retvect.push_back (a[i] / b[i]);
+    }
+    return retvect;
+}
+
+inline std::vector<float> operator+ (std::vector<float> a, std::vector<float> b)
+{
+    std::vector<float> retvect;
+    for (int i = 0; i < a.size() ; i++)
+    {
+        retvect.push_back (a[i] + b[i]);
     }
     return retvect;
 }
